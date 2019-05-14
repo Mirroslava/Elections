0bfcd2a7eb770f7d274ea45b037c7e22d8c1c077
Oleg <gitgudcasuls@gmail.com>
2019-05-15 01:25:41 +0300

    +admin page

--- ElectionLand/Controllers/CabinetController.cs
+++ ElectionLand/Controllers/CabinetController.cs
@@ -9,36 +9,58 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace ElectionLand.Controllers
 {
+
     public class CabinetController : Controller
     {
-        AplicationContext db;
-
+        static AplicationContext db;
+        static int currentUserId;
 
         [HttpGet]
         public IActionResult LogIn()
         {
+            currentUserId = new int();
             ViewBag.Status = "";
             return View("LogIn");
         }
         [HttpPost]
         public IActionResult LogIn(string login, string password)
         {
+            if (login == "admin1" && password == "admin1")
+            {
+                return RedirectToAction("Admin", "Admin");
+            }
             var user = db.Users.Where(u => u.Login == login && u.Password == password);
-            if (user.Count()==0)
+            if (user.Count() == 0)
             {
                 ViewBag.Status = "Облікового запису з таким логіном та паролем ще не було створено!";
                 return View("LogIn");
             }
-                
-            else return View("Index", user);
+            else
+            {
+                currentUserId = user.Last().Id;
+                try
+                {
+                    var districtId = db.UsetToVirtualDistricts.Where(d => d.UserId == user.Last().Id).Last().VirtualDistrictId;
+                    var district = db.VirtualDistricts.Where(d => d.Id == districtId);
+
+                    ViewBag.DistrictTitle = district.Last().Title;
+                    ViewBag.DistrictAddress = district.Last().Adress;
+                }
+                catch { }
+
+                return RedirectToAction("Index");
+
+            }
+
         }
         [HttpGet]
         public IActionResult Registration()
         {
+            currentUserId = new int();
             return View("Registration");
         }
         [HttpPost]
-        public IActionResult Registration(User user)
+        public IActionResult Registration(User user, int districtId)
         {
             if (!ModelState.IsValid)
             {
@@ -48,9 +70,11 @@ namespace ElectionLand.Controllers
             db.Users.Add(user);
             db.UserToRoles.Add(new UserToRole { Id = db.UserToRoles.Count() + 1, UserId = user.Id, RoleId = 1 });
             db.StatusToUsers.Add(new StatusToUser { Id = db.StatusToUsers.Count() + 1, UserId = user.Id, UserStatusId = 1 });
+            db.UsetToVirtualDistricts.Add(new UsetToVirtualDistrict { Id = db.UsetToVirtualDistricts.Count() + 1, UserId = user.Id, VirtualDistrictId = districtId });
             db.SaveChanges();
             var user1 = db.Users.Where(u => u.Id == user.Id);
-            return View("Index", user1);
+            currentUserId = user.Id;
+            return RedirectToAction("Index");
         }
         #region BirthDate
         public JsonResult BirthDateCheck(DateTime BirthDate)
@@ -64,9 +88,9 @@ namespace ElectionLand.Controllers
         }
         #endregion
         public IActionResult UniquePINCheck(long pin)
-        { 
+        {
             if (db.Users.Where(u => u.PIN == pin).Any()) return Json("Користувач з таким кодом вже був зареєстрований");
-            else return Json(true) ;
+            else return Json(true);
         }
 
         [HttpPost]
@@ -83,29 +107,48 @@ namespace ElectionLand.Controllers
         }
         public IActionResult Bulletin()
         {
-           
+
             return View();
         }
         public IActionResult Index()
         {
-            int userId = 1;
+            int userId = currentUserId;
             var user = db.Users.Where(us => us.Id == userId);
+            try
+            {
+                var districtId = db.UsetToVirtualDistricts.Where(d => d.UserId == user.Last().Id).Last().VirtualDistrictId;
+                var district = db.VirtualDistricts.Where(d => d.Id == districtId);
+
+                ViewBag.DistrictTitle = district.Last().Title;
+                ViewBag.DistrictAddress = district.Last().Adress;
+            }
+            catch { }
             return View(user);
         }
+        [HttpGet]
         public IActionResult Edite()
         {
-            int userId = 1;
-           User user = db.Users.FirstOrDefault(p => p.Id == userId);
 
-             return View(user);
-           
+            int userId = currentUserId;
+            var user = db.Users.Where(u => u.Id == currentUserId).Last();
+
+            return View("Edite", user);
         }
         [HttpPost]
-        public IActionResult Edite(User user)
+        public IActionResult Edite(User userChanges)
         {
-            db.Users.Update(user);
+            var currentUser = db.Users.Where(u => u.Id == currentUserId).Last();
+
+            currentUser.FirstName = userChanges.FirstName;
+            currentUser.LastName = userChanges.LastName;
+            currentUser.Email = userChanges.Email;
+
+            db.Users.Update(currentUser);
             db.SaveChangesAsync();
+
+            var user = db.Users.Where(us => us.Id == currentUser.Id);
             return RedirectToAction("Index");
+
         }
     }
 }
--- ElectionLand/Models/User.cs
+++ ElectionLand/Models/User.cs
@@ -13,26 +13,27 @@ namespace ElectionLand.Models
     {
         [Key]
         public int Id { get; set; }
-        [Required(ErrorMessage = "Будь ласка, вкажість ваше ім'я")]
+        [Required(ErrorMessage = "Будь ласка, вкажіть ваше ім'я")]
         public string FirstName { get; set; }
-        [Required(ErrorMessage = "Будь ласка, вкажість ваше прізвище")]
+        [Required(ErrorMessage = "Будь ласка, вкажіть ваше прізвище")]
         public string LastName { get; set; }
 
-        [Required(ErrorMessage = "Будь ласка, вкажість дату вашого народження")]
+        [Required(ErrorMessage = "Будь ласка, вкажіть дату вашого народження")]
         [Remote(action: "BirthDateCheck", controller:"Cabinet")]
         public DateTime BirthDate { get; set; }
 
-        [Required(ErrorMessage = "Будь ласка, вкажість ваш код ")]
+        [Required(ErrorMessage = "Будь ласка, вкажіть ваш код ")]
         [Remote(action:"UniquePINCheck", controller:"Cabinet")]
+        [Range(1000000000000, 9999999999999, ErrorMessage = "Код містить 13 цифр")]
         public long PIN { get; set; }
 
         public string Email { get; set; }
 
-        [Required(ErrorMessage = "Будь ласка, вкажість ваш логін")]
-        [Remote(action:"UniqueLoginCheck", controller:"Cabinet", HttpMethod ="POST",ErrorMessage ="Account with such login has already been created")]
+        [Required(ErrorMessage = "Будь ласка, вкажіть ваш логін")]
+        [Remote(action:"UniqueLoginCheck", controller:"Cabinet", HttpMethod ="POST",ErrorMessage ="Кабінет з таким логіном вже був створений")]
         public string Login { get; set; }
 
-        [Required(ErrorMessage = "Будь ласка, вкажість ваш пароль")]
+        [Required(ErrorMessage = "Будь ласка, вкажіть ваш пароль")]
         [StringLength(30, MinimumLength =6 ,ErrorMessage ="Пароль має вміщувати хоча б 6 символів")]
         public string Password { get; set; }
 
